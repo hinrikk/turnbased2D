3b48e4f5c14cc0737c125078314e62268bbfcef8
Hinrik <hinrik.koennecker@gmail.com>
2026-05-23 10:43:23 +0200

    Enemy Health

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,11 +3,34 @@ using UnityEngine.Tilemaps;
 
 public class Enemy : MonoBehaviour
 {
+    public int health = 10;
+    TileNode currentNode;
+
     void Start()
     {
         // Occupy tiles to make them unreachable
         Vector3Int cell = Vector3Int.RoundToInt(transform.position);
         Debug.Log($"Enemy Pos: {cell}");
-        GridManager.Instance.GetNode(cell).occupied = true;
+        currentNode = GridManager.Instance.GetNode(cell);
+        currentNode.occupied = true;
+    }
+
+    public void TakeDamage(int amount)
+    {
+        health -= amount;
+
+        Debug.Log(
+            $"Enemy HP: {health}");
+
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        currentNode.occupied = false;
+        Destroy(gameObject);
     }
 }
